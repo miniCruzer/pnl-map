b0563c713f163451c9e486aac83ff958815d6889
Samuel Hoffman <samuelhoffman2@gmail.com>
2017-11-30 20:14:04 -0600

    Created a main window for the program.

--- main.pyw
+++ main.pyw
@@ -2,17 +2,39 @@
 
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from src.wizard import Wizard
+from src.ui.mainwindow import Ui_MainWindow
+from src.mapper import MapEditor
+
+
+class MainWindow(Ui_MainWindow, QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.wizardCmd.clicked.connect(self.launchWizard)
+        self.mapEditCmd.clicked.connect(self.launchMapEditor)
+
+    def launchWizard(self):
+        """ launch the conversion wizard """
+        wiz = Wizard(self)
+        wiz.exec_()
+
+    def launchMapEditor(self):
+        """ launch the map editor """
+        dialog = MapEditor(self)
+        dialog.exec_()
 
 
 def launch_gui():
     """ launch the GUI application and ignore all other command line arguments """
 
     app = QApplication(sys.argv)
-    wizard = Wizard()
-    wizard.show()
+    mw = MainWindow()
+    mw.show()
 
     sys.exit(app.exec_())
 
---
+++ src/ui/mainwindow.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui\mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(600, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.wizardCmd = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.wizardCmd.setObjectName("wizardCmd")
+        self.verticalLayout.addWidget(self.wizardCmd)
+        self.mapEditCmd = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.mapEditCmd.setObjectName("mapEditCmd")
+        self.verticalLayout.addWidget(self.mapEditCmd)
+        self.templateCmd = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.templateCmd.setObjectName("templateCmd")
+        self.verticalLayout.addWidget(self.templateCmd)
+        self.quitCmd = QtWidgets.QCommandLinkButton(self.centralwidget)
+        self.quitCmd.setObjectName("quitCmd")
+        self.verticalLayout.addWidget(self.quitCmd)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 600, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.quitCmd.clicked.connect(MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Popeyes P&L Converter"))
+        self.wizardCmd.setText(_translate("MainWindow", "Conversion wizard"))
+        self.wizardCmd.setDescription(_translate("MainWindow", "Convert a QuickBooks export to the Popeyes format."))
+        self.mapEditCmd.setText(_translate("MainWindow", "Map files"))
+        self.mapEditCmd.setDescription(_translate("MainWindow", "Create and edit company map files."))
+        self.templateCmd.setText(_translate("MainWindow", "Spreadsheet templates"))
+        self.templateCmd.setDescription(_translate("MainWindow", "Set up templates."))
+        self.quitCmd.setText(_translate("MainWindow", "Quit"))
+        self.quitCmd.setDescription(_translate("MainWindow", "Exit program."))
+
